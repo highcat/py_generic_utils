07412726565338d057c2b226667724afa3733f4f
HighCat <highcatland@gmail.com>
2011-12-23 16:26:29 +0400

    minor - rename patched class

--- py_generic_utils/timedelta_py26_patch.py
+++ py_generic_utils/timedelta_py26_patch.py
@@ -3,7 +3,7 @@ import datetime
 
 # patch timedelta in python 2.6 to behave like python 2.7
 if sys.version_info < (2, 7):
-    class my_td(datetime.timedelta):
+    class timedelta_patched(datetime.timedelta):
         def total_seconds(self):
             return (float(self.microseconds) + (self.seconds + self.days * 24 * 3600) * 10**6) / 10**6
-    datetime.timedelta = my_td
+    datetime.timedelta = timedelta_patched
